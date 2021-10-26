eef3e9052c7aeca15855836a3426b47f9236ddf4
apoorvkh <apoorv.khand@gmail.com>
2021-10-26 14:26:04 +0000

    Removing redundant block

--- habitat_baselines/rl/ddppo/policy/resnet_objgoal_policy.py
+++ habitat_baselines/rl/ddppo/policy/resnet_objgoal_policy.py
@@ -18,6 +18,7 @@ from habitat.tasks.nav.nav import (
 )
 from habitat.tasks.nav.object_nav_task import ObjectGoalSensor
 from habitat_baselines.common.baseline_registry import baseline_registry
+from habitat_baselines.rl.ddppo.policy.resnet_policy import ResNetEncoder
 from habitat_baselines.rl.ddppo.policy import resnet
 from habitat_baselines.rl.ddppo.policy.running_mean_and_var import (
     RunningMeanAndVar,
@@ -72,102 +73,6 @@ class ObjGoalResNetPolicy(Policy):
             backbone=config.RL.DDPPO.backbone,
             normalize_visual_inputs="rgb" in observation_space.spaces,
         )
-
-
-class ResNetEncoder(nn.Module):
-    def __init__(
-        self,
-        observation_space: spaces.Dict,
-        baseplanes: int = 32,
-        ngroups: int = 32,
-        spatial_size: int = 128,
-        make_backbone=None,
-        normalize_visual_inputs: bool = False,
-    ):
-        super().__init__()
-
-        if "rgb" in observation_space.spaces:
-            self._n_input_rgb = observation_space.spaces["rgb"].shape[2]
-            spatial_size = observation_space.spaces["rgb"].shape[0] // 2
-        else:
-            self._n_input_rgb = 0
-
-        if "depth" in observation_space.spaces:
-            self._n_input_depth = observation_space.spaces["depth"].shape[2]
-            spatial_size = observation_space.spaces["depth"].shape[0] // 2
-        else:
-            self._n_input_depth = 0
-
-        if normalize_visual_inputs:
-            self.running_mean_and_var: nn.Module = RunningMeanAndVar(
-                self._n_input_depth + self._n_input_rgb
-            )
-        else:
-            self.running_mean_and_var = nn.Sequential()
-
-        input_channels = self._n_input_depth + self._n_input_rgb
-        self.backbone = make_backbone(input_channels, baseplanes, ngroups)
-
-        final_spatial = int(
-            spatial_size * self.backbone.final_spatial_compress
-        )
-        after_compression_flat_size = 2048
-        num_compression_channels = int(
-            round(after_compression_flat_size / (final_spatial ** 2))
-        )
-        self.compression = nn.Sequential(
-            nn.Conv2d(
-                self.backbone.final_channels,
-                num_compression_channels,
-                kernel_size=3,
-                padding=1,
-                bias=False,
-            ),
-            nn.GroupNorm(1, num_compression_channels),
-            nn.ReLU(True),
-        )
-
-        self.output_shape = (
-            num_compression_channels,
-            final_spatial,
-            final_spatial,
-        )
-
-    def layer_init(self):
-        for layer in self.modules():
-            if isinstance(layer, (nn.Conv2d, nn.Linear)):
-                nn.init.kaiming_normal_(
-                    layer.weight, nn.init.calculate_gain("relu")
-                )
-                if layer.bias is not None:
-                    nn.init.constant_(layer.bias, val=0)
-
-    def forward(self, observations: Dict[str, torch.Tensor]) -> torch.Tensor:  # type: ignore
-        cnn_input = []
-        if self._n_input_rgb > 0:
-            rgb_observations = observations["rgb"]
-            # permute tensor to dimension [BATCH x CHANNEL x HEIGHT X WIDTH]
-            rgb_observations = rgb_observations.permute(0, 3, 1, 2)
-            rgb_observations = (
-                rgb_observations.float() / 255.0
-            )  # normalize RGB
-            cnn_input.append(rgb_observations)
-
-        if self._n_input_depth > 0:
-            depth_observations = observations["depth"]
-
-            # permute tensor to dimension [BATCH x CHANNEL x HEIGHT X WIDTH]
-            depth_observations = depth_observations.permute(0, 3, 1, 2)
-
-            cnn_input.append(depth_observations)
-
-        x = torch.cat(cnn_input, dim=1)
-        x = F.avg_pool2d(x, 2)
-
-        x = self.running_mean_and_var(x)
-        x = self.backbone(x)
-        x = self.compression(x)
-        return x
 
 
 class ResnetTensorGoalEncoder(nn.Module):
