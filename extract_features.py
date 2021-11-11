e6eede89e233e1ed87ee6c8d2deca6d3b7e5b281
apoorvkh <apoorv.khand@gmail.com>
2021-11-11 06:01:47 -0800

    Dataset generation

---
+++ extract_features.py
@@ -0,0 +1,108 @@
+import os
+from glob import glob
+
+from PIL import Image
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import transforms as T
+from torchvision import models
+import clip
+
+from constants import target_objects
+
+
+data_dir = os.path.expanduser('~/nfs/clip-embodied-ai/datasets/ithor_scenes')
+
+
+def freeze_model(model):
+    for param in model.parameters():
+        param.requires_grad = False
+    for module in model.modules():
+        if "BatchNorm" in type(module).__name__:
+            module.momentum = 0.0
+    model.eval()
+    return model
+
+
+resnet_preprocess = T.Compose([
+    T.ToTensor(),
+    T.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+resnet_model = models.resnet50(pretrained=True)
+resnet_model.fc = nn.Identity()
+resnet_model = freeze_model(resnet_model)
+resnet_model = resnet_model.cuda()
+
+
+clip_model, clip_preprocess = clip.load('RN50', device=torch.device('cuda'))
+clip_model = clip_model.visual
+clip_model.attnpool = nn.Sequential(
+    nn.AdaptiveAvgPool2d(output_size=(1,1)),
+    nn.Flatten()
+)
+clip_model = freeze_model(clip_model)
+
+
+def class_mask(semantic_frame, class_color):
+    if class_color is None:
+        return np.zeros(semantic_frame.shape[:2], dtype=bool)
+    mask = np.all(semantic_frame == class_color, axis=-1)
+    return mask
+
+def obj_presence(class_masks):
+    return class_masks.sum(axis=(1,2)) > 0
+
+def grid_bboxes(image_shape, grid_sizes):
+    for i in range(grid_sizes[0]):
+        for j in range(grid_sizes[1]):
+            yield (
+                int(i * image_shape[0] / grid_sizes[0]),
+                int((i+1) * image_shape[0] / grid_sizes[0]),
+                int(j * image_shape[1] / grid_sizes[1]),
+                int((j+1) * image_shape[1] / grid_sizes[1]),
+            )
+
+
+for split in ['train', 'val', 'test']:
+
+    features = {}
+    for scene in glob(os.path.join(data_dir, split, '*.npy')):
+        print(scene)
+        features[scene] = []
+        data = np.load(scene, allow_pickle=True)
+        for point in data:
+            frame = Image.fromarray(point['frame'])
+
+            resnet_input = resnet_preprocess(frame).unsqueeze(0).cuda()
+            resnet_features = resnet_model(resnet_input)[0].cpu()
+
+            clip_input = clip_preprocess(frame).unsqueeze(0).cuda()
+            clip_features = clip_model(clip_input).float()[0].cpu()
+
+            class_masks = np.array([
+                class_mask(
+                    point['semantic_frame'],
+                    point['object_id_to_color'].get(o, None)
+                ) for o in target_objects
+            ])
+
+            object_presence = torch.tensor(obj_presence(class_masks), dtype=int, device=torch.device('cpu'))
+            object_presence_grid = torch.tensor(
+                [obj_presence(class_masks[:, y1:y2, x1:x2]) for (y1, y2, x1, x2) in grid_bboxes(class_masks.shape[1:3], (3, 3))],
+                dtype=int,
+                device=torch.device('cpu')
+            )
+
+            features[scene].append({
+                'rn50_imagenet' : resnet_features,
+                'clip' : clip_features,
+                'object_presence' : object_presence,
+                'object_presence_grid' : object_presence_grid,
+                'valid_moves_forward' : point['valid_moves_forward']
+            })
+
+    torch.save(features, os.path.join(data_dir, f"{split}.npy"))
---
+++ generate_thor_frames.py
@@ -0,0 +1,98 @@
+# generated with ai2thor 3.5.1
+
+import os
+import cv2
+import random
+
+import numpy as np
+from ai2thor.controller import Controller
+
+from constants import target_objects
+
+
+output_dir = os.path.expanduser('~/nfs/clip-embodied-ai/datasets/ithor_scenes')
+os.makedirs(os.path.join(output_dir, 'train'), exist_ok=True)
+os.makedirs(os.path.join(output_dir, 'val'), exist_ok=True)
+os.makedirs(os.path.join(output_dir, 'test'), exist_ok=True)
+
+
+controller = Controller(
+    gridSize=0.25,
+    makeAgentsVisible=False,
+    rotateStepDegrees=90,
+    renderDepthImage=True,
+    renderSemanticSegmentation=True,
+    renderInstanceSegmentation=True,
+    quality='High',
+    width=300,
+    height=300,
+    fieldOfView=90
+)
+
+scenes = controller.ithor_scenes(include_bathrooms=False)
+
+for scene_name in scenes:
+    print(scene_name)
+
+    scene_id = int(scene_name.replace('FloorPlan', '').replace('_physics', ''))
+    if scene_id % 100 <= 20:
+        split = 'train'
+    elif scene_id % 100 <= 25:
+        split = 'val'
+    else:
+        split = 'test'
+
+    controller.reset(scene=scene_name)
+    controller.step(action="GetReachablePositions")
+    locations = controller.last_event.metadata["actionReturn"][:]
+    rotations = [0, 90, 180, 270]
+    horizons = [45]
+
+    data = []
+    while len(data) < (100 if split == 'train' else 50):
+        pos = random.sample(locations, 1)[0]
+        rot, hor = None, None
+
+        tries = 0
+        while tries < 4:
+            tries += 1
+
+            rot = random.sample(rotations, 1)[0]
+            hor = random.sample(horizons, 1)[0]
+            e = controller.step(
+                action="TeleportFull",
+                position=pos,
+                rotation=dict(x=0, y=rot, z=0),
+                horizon=hor,
+                standing=True
+            )
+
+            object_mask = np.any([v for k,v in e.class_masks.items() if k in target_objects], axis=0)
+            object_frac = np.sum(object_mask) / np.prod(object_mask.shape)
+
+            if object_frac > 0.015:
+                break
+        else:
+            continue
+
+        valid_moves_forward = 0
+        while controller.step('MoveAhead').metadata['lastActionSuccess']:
+            valid_moves_forward += 1
+
+        data.append({
+            'agent_metadata' : {
+                'position' : pos,
+                'rotation' : dict(x=0, y=rot, z=0),
+                'horizon' : hor,
+                'standing' : True
+            },
+            'object_metadata' : e.metadata['objects'],
+            'frame' : e.frame,
+            'depth_frame' : e.depth_frame,
+            'semantic_frame' : e.semantic_segmentation_frame,
+            'instance_frame' : e.instance_segmentation_frame,
+            'object_id_to_color' : e.object_id_to_color,
+            'valid_moves_forward' : valid_moves_forward
+        })
+
+    np.save(os.path.join(output_dir, split, f"{scene_name}.npy"), data)
