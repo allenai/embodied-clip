e81df35ba5b1ec8fa3bce2be92d134e71156914e
apoorvkh <apoorv.khand@gmail.com>
2022-03-29 22:21:50 -0700

    Updating install with conda

--- setup.py
+++ setup.py
@@ -22,66 +22,19 @@ with open("README.md", encoding="utf8") as f:
 with open("LICENSE") as f:
     license_text = f.read()
 
-with open("requirements.txt") as f:
-    reqs = f.read()
-
 DISTNAME = "habitat"
 DESCRIPTION = "habitat: a suite for embodied agent tasks and benchmarks"
 LONG_DESCRIPTION = readme
 AUTHOR = "Facebook AI Research"
 LICENSE = license_text
-REQUIREMENTS = reqs.strip().split("\n")
-BASELINE_PATH = ["habitat_baselines", "habitat_baselines.*"]
 DEFAULT_EXCLUSION = ["test", "examples"]
-FULL_REQUIREMENTS = set()
-# collect requirements.txt file in all subdirectories
-for file_name in ["requirements.txt"] + glob.glob(
-    "habitat_baselines/**/requirements.txt", recursive=True
-):
+
+REQUIREMENTS = set()
+for file_name in glob.glob("habitat_baselines/**/requirements.txt", recursive=True):
     with open(file_name) as f:
         reqs = f.read()
-        FULL_REQUIREMENTS.update(reqs.strip().split("\n"))
-
-
-class OptionedCommand:
-    r"""Generic Command class that takes extra user options and modifies
-    arguments in setuptools.setup() accordingly.
-    Though OptionedCommand inherits directly from object, it assumes
-    inheritance from DefaultDevelopCommand or DefaultInstallCommand, as it
-    overrides methods from those two classes.
-    """
-
-    user_options = [("all", None, "include habitat_baselines in installation")]
-
-    def initialize_options(self):
-        super().initialize_options()
-        self.all = None
-
-    def run(self):
-        if not self.all:  # install core only
-            DEFAULT_EXCLUSION.extend(BASELINE_PATH)
-            self.distribution.packages = setuptools.find_packages(
-                exclude=DEFAULT_EXCLUSION
-            )
-            # self.distribution accesses arguments of setup() in main()
-        else:  # install all except test and examples
-            self.distribution.install_requires = FULL_REQUIREMENTS
-        super().run()
-
-
-class InstallCommand(OptionedCommand, DefaultInstallCommand):
-    user_options = (
-        getattr(DefaultInstallCommand, "user_options", [])
-        + OptionedCommand.user_options
-    )
-
-
-class DevelopCommand(OptionedCommand, DefaultDevelopCommand):
-    user_options = (
-        getattr(DefaultDevelopCommand, "user_options", [])
-        + OptionedCommand.user_options
-    )
-
+        REQUIREMENTS.update(reqs.strip().split("\n"))
+REQUIREMENTS = list(REQUIREMENTS)
 
 if __name__ == "__main__":
     setuptools.setup(
@@ -96,5 +49,5 @@ if __name__ == "__main__":
         setup_requires=["pytest-runner"],
         tests_require=["pytest-cov", "pytest-mock", "pytest"],
         include_package_data=True,
-        cmdclass={"install": InstallCommand, "develop": DevelopCommand},
+        cmdclass={"install": DefaultInstallCommand, "develop": DefaultDevelopCommand},
     )
